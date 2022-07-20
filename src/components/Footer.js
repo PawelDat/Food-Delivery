dbadf8f377e17e5d39772141f537af4051db3199
PawelDat <pawel.datko2000@gmail.com>
2022-07-20 15:34:48 +0200

    Completed Footer Section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,89 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../assets/images/icons/logo.png";
+
+
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+              <p>
+                The best food delivery Company.
+              </p>
+            </div>
+          </Col>
 
-export default Footer
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Monday - Thursday</span>
+                <p>10:00am - 12:00pm</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday - Sunday</span>
+                <p>14:00am - 12:00pm</p>
+              </ListGroupItem>
+
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Katowice, Poland</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 002003004</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: food@delivery.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2022, website made by Pawel Datko. All Rights
+              Reserved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+              <span>
+                <Link to="https://github.com/PawelDat">
+                  <i class="ri-github-line"></i>
+                </Link>
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
